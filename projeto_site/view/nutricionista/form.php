da261f50649da9ccb6fc3b79b0939f98a619e0bd
ProjetoDelimeterSite <ProjetoDelimeterSite@gmail.com>
2025-06-30 16:47:22 +0000

    update

--- projeto_site/view/nutricionista/form.php
+++
@@ -1,7 +0,0 @@
-    <h1>Cadastro de Nutricionista</h1>
-    <form action="/api/nutricionista" method="post">
-        <label for="crm_nutricionista">CRM Nutricionista:</label>
-        <input type="text" id="crm_nutricionista" name="crm_nutricionista" required><br><br>
-
-        <button type="submit">Salvar</button>
-    </form>
